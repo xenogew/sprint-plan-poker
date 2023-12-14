104fd0734de518e2d761bcef8e32ebb87cb41c33
WangAphisit Nattasan <ts-wangaphi.nattasan@rakuten.com>
2023-12-14 20:44:03 +0900

    setup UI and settings

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+	import LightSwitch from "$lib/components/ui/LightSwitch.svelte";
+</script>
+
+<LightSwitch />
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
